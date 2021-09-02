f16835e38de3d00f0826062e97d7b413657a391d
Maksim <max.alekseev04@gmail.com>
2021-09-02 14:59:46 +0300

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import styles from './pages/HomePage/HomePage.module.scss';
-import HomePage from './pages/HomePage';
-import ComponentsPage from './pages/ComponentsPage';
-import NotFound from './components/NotFound';
+import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import styles from './pages/HomePage/HomePage.module.scss'
+import HomePage from './pages/HomePage'
+import ComponentsPage from './pages/ComponentsPage'
+import NotFound from './components/NotFound'
 
-function App() {
+function App () {
   return (
     <>
       <Router>
         <div className={styles.headerContainer}>
-          <div className={styles.pagesContainer}>
-            <ul>
-              <li>
-                <Link to={'/'}>Home</Link>
-              </li>
-              <li>
-                <Link to={'/components'}>Components</Link>
-              </li>
-            </ul>
-          </div>
-          <div className={styles.headerTextContainer}>
-            <h1 className={styles.headerText}>My Components</h1>
-          </div>
+          <nav className={styles.pagesContainer}>
+            <div className={styles.textContainer}>
+              <div>
+                <Link to={'/'} className={styles.text}>
+                  Home
+                </Link>
+              </div>
+              <div>
+                <Link to={'/components'} className={styles.text}>
+                  Components
+                </Link>
+              </div>
+            </div>
+            <div className={styles.headerTextContainer}>
+              <h1 className={styles.headerText}>My Components</h1>
+            </div>
+          </nav>
         </div>
         <Switch>
           <Route exact path={'/'}>
@@ -36,7 +40,7 @@ function App() {
         </Switch>
       </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/ComponentsPage/index.jsx
+++ src/pages/ComponentsPage/index.jsx
@@ -1,33 +1,39 @@
-import React from 'react';
-import { Switch, Route, Link, useRouteMatch } from 'react-router-dom';
-import styles from './../../pages/HomePage/HomePage.module.scss';
-import Calendar from '../../components/Calendar';
-import LoginForm from '../../components/LoginForm';
-import NotFound from '../../components/NotFound';
-import UsersListHW from '../../components/UsersListHW';
+import React from 'react'
+import { Switch, Route, Link, useRouteMatch } from 'react-router-dom'
+import styles from './../../pages/HomePage/HomePage.module.scss'
+import Calendar from '../../components/Calendar'
+import LoginForm from '../../components/LoginForm'
+import NotFound from '../../components/NotFound'
+import UsersListHW from '../../components/UsersListHW'
 
-function ComponentsPage() {
-  const { path, url } = useRouteMatch();
+function ComponentsPage () {
+  const { path, url } = useRouteMatch()
 
   return (
     <div className={styles.componentsPage}>
       <div className={styles.componentsContainer}>
-        <div className={styles.componentsPageContainer}>
-          <ul>
-            <li>
-              <Link to={`${url}/usersList`}>Users List</Link>
-            </li>
-            <li>
-              <Link to={`${url}/loginForm`}>Login Form</Link>
-            </li>
-            <li>
-              <Link to={`${url}/calendar`}>Calendar</Link>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.componentsContainerText}>
-          <h1 className={styles.componentsText}>Click on any project</h1>
-        </div>
+        <nav className={styles.componentsPageContainer}>
+          <div className={styles.textContainer}>
+            <div>
+              <Link to={`${url}/usersList`} className={styles.text}>
+                Users List
+              </Link>
+            </div>
+            <div>
+              <Link to={`${url}/loginForm`} className={styles.text}>
+                Login Form
+              </Link>
+            </div>
+            <div>
+              <Link to={`${url}/calendar`} className={styles.text}>
+                Calendar
+              </Link>
+            </div>
+          </div>
+          <div className={styles.componentsContainerText}>
+            <h1 className={styles.componentsText}>Click on any project</h1>
+          </div>
+        </nav>
       </div>
       <Switch>
         <Route path={`${path}/usersList`}>
@@ -42,7 +48,7 @@ function ComponentsPage() {
         <Route path={`${path}/*`} component={NotFound} />
       </Switch>
     </div>
-  );
+  )
 }
 
-export default ComponentsPage;
+export default ComponentsPage
